10c54fdb0cea7f7e84bf31afa073e9d3c5c213ba
muhtadi-ridwan <muhtadiridwan5@gmail.com>
2024-04-02 01:26:10 +0600

    updated nav2.js

--- nav2/nav2.js
+++ nav2/nav2.js
@@ -1,15 +1,17 @@
 function changeContent(type) {
     var contentDiv = document.getElementById('content');
     switch (type) {
+      
       case 'post':
 
         fetch('/SAD-SIUM/post.html')
         .then(response => response.text())
         .then(data => contentDiv.innerHTML = data)
         .catch(error => console.error('Failed to load HTML content:', error));
-       
+        
         break;
       
+        
         case 'paper':
           fetch('/SAD-SIUM/paper.html')
           .then(response => response.text())
@@ -17,6 +19,7 @@ function changeContent(type) {
           .catch(error => console.error('Failed to load HTML content:', error));
         break;
       
+        
         case 'question':
           fetch('/SAD-SIUM/question.html')
           .then(response => response.text())
@@ -24,6 +27,7 @@ function changeContent(type) {
           .catch(error => console.error('Failed to load HTML content:', error));
         break;
       
+        
         case 'job post':
           fetch('/SAD-SIUM/job.html')
           .then(response => response.text())
@@ -31,11 +35,13 @@ function changeContent(type) {
           .catch(error => console.error('Failed to load HTML content:', error));
         break;
       
+        
         case 'requisition post':
           fetch('/SAD-SIUM/requisitionpost.html')
           .then(response => response.text())
           .then(data => contentDiv.innerHTML = data)
           .catch(error => console.error('Failed to load HTML content:', error));
+        
         break;
       default:
         break;
